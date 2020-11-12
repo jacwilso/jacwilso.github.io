85b512cef22186565411fd6a777f49790fdea983
Jacob Wilson <jacwilso0@gmail.com>
2020-11-11 23:06:58 -0800

    Converted image gallery to slideshows

--- script.js
+++ script.js
@@ -31,6 +31,36 @@ window.onclick = function(event) {
     }
 }
 
+var slideIndex = [];
 document.addEventListener("DOMContentLoaded", function(event) {
     modal = document.getElementById("modal-images");
+    var slideshows = document.getElementsByClassName("slideshow");
+    for (shows = 0; shows < slideshows.length; shows++) {
+        slideIndex.push(1);
+        showSlides(1, shows);
+    }
 });
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
+}
+
+function showSlides(n, no) {
+    var slideshow = document.getElementsByClassName("slideshow")[no];
+    var slides = slideshow.getElementsByClassName("slide-container");
+    var dots = slideshow.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = slides.length}
+  for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+  }
+  slides[slideIndex[no]-1].style.display = "flex";
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex[no]-1].className += " active";
+}
