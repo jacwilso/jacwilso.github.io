a23ab145b5b27dc0c351600bdc6c96136ee13512
Jacob Wilson <jacwilso@Jacobs-MacBook-Pro.local>
2021-01-04 12:21:36 -0800

    Fixed videos on chrome

--- script.js
+++ script.js
@@ -2,22 +2,15 @@ var modal = null;
 
 function openModal(element) {
     modal.style.display = "block";
-    // Reset modal
-    var imgsHTML = modal.getElementsByTagName("img");
     var descriptionHTML = document.getElementById("modal-description");
     descriptionHTML.innerHTML = "";
-    for (var i=0; i<9; ++i) {
-        imgsHTML[i].src = "";
-    }
-    // Set modal
     var description = element.dataset.description;
     if (description) {
         descriptionHTML.innerHTML = description;
     }
-    var images = element.dataset.images.replaceAll('\n','').replaceAll('\t','').split(',');
-    for (var i=0; i<images.length; ++i) {
-        imgsHTML[3*(i%3) + Math.floor(i/3)].src = images[i];
-    }
+    
+    var gridHTML = document.getElementById("modal-grid");
+    gridHTML.innerHTML = element.dataset.html;
 }
 
 function closeModal() {
